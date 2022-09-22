cc31c2e3ff81dbad54a171472b0e811624a6ce6f
Roshanylanke <113982181+Roshanylanke@users.noreply.github.com>
2022-09-22 09:01:11 +0530

    Add files via upload

---
+++ IsPrime.java
@@ -0,0 +1,22 @@
+package week1.day2;
+
+public class IsPrime {
+
+	public static void main(String[] args) {
+	
+	
+			int n=2;
+			System.out.println(n);
+			
+		if(n%2 == 0) {
+			System.out.println("is a prime number");
+		}
+		else {
+			System.out.println("is not a prime number");
+		}
+
+	}
+	}
+
+
+
